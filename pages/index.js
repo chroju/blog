ada3f2fd6735420576897964da454535859d7592
chroju <chroju@users.noreply.github.com>
2020-12-28 13:48:39 +0900

    Add pre rendering pages

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,18 @@ import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilsStyles from '../styles/utils.module.css'
 
-export default function Home() {
+import { getSortedPostsData } from '../lib/posts'
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -14,6 +25,20 @@ export default function Home() {
           (This is a sample website - you'll be building a state like this on{' '})
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
+      </section>
+      <section className={`${utilsStyles.headingMd} ${utilsStyles.padding1px}`}>
+        <h2 className={utilsStyles.headingLg}>Blog</h2>
+        <ul className={utilsStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilsStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   )
