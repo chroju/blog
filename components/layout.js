fd1d0df2335fad51f92aaf36edbbca1fe687c74e
chroju <chroju@users.noreply.github.com>
2020-12-31 10:30:25 +0900

    Add favicon

--- components/layout.js
+++ components/layout.js
@@ -12,7 +12,12 @@ export default function Layout({ children, home }) {
     return (
         <div className={styles.container}>
             <Head>
-                <link rel="icon" href="/favicon.ico" />
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+                <meta name="msapplication-TileColor" content="#da532c" />
+                <meta name="theme-color" content="#ffffff" />
                 <link rel="alternate" type="application/xml" href="https://chroju.dev/feed.xml" title="RSS 2.0" />
                 <meta
                     name="description"
