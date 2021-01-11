0310b56b03409cdea8e847b77ce3641cf04c113a
chroju <chroju@users.noreply.github.com>
2021-01-11 21:49:14 +0900

    Modify og image URL

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -37,9 +37,7 @@ export default function Post({ postData }) {
                 <title>{pageTitle}</title>
                 <meta
                     property="og:image"
-                    content={`https://og-image.chroju.dev/${encodeURI(
-                        postData.title
-                    )}.png?theme=dark&md=0&fontSize=75px`}
+                    content={`https://og-image.chroju.dev/${postData.title}.png?theme=dark&md=0&fontSize=75px`}
                 />
                 <meta name="og:title" content={pageTitle} />
                 <meta name="og:url" content={pageURL} />
