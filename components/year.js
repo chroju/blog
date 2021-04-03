aeeea35514373ec89315e3c99a313e50c5cb22e1
chroju <chroju@users.noreply.github.com>
2021-04-03 10:51:27 +0900

    Update created date displays

---
+++ components/year.js
@@ -0,0 +1,3 @@
+export default function Year({ year }) {
+    return year !== null ? <h3>{year}</h3> : null
+}
--- pages/blog/index.js
+++ pages/blog/index.js
@@ -3,6 +3,7 @@ import Layout, { siteTitle } from '../../components/layout'
 import utilsStyles from '../../styles/utils.module.css'
 import Link from 'next/link'
 import Date from '../../components/date'
+import Year from '../../components/year'
 
 import { getSortedPostsData } from '../../lib/posts'
 
@@ -29,10 +30,12 @@ export default function Home({ allPostsData }) {
         <meta name="og:title" content={siteTitle} />
         <meta name="og:url" content="https://chroju.dev/blog" />
       </Head>
+      <h2>Articles</h2>
       <section className={`${utilsStyles.headingMd} ${utilsStyles.padding1px}`}>
         <ul className={utilsStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
+          {allPostsData.map(({ id, date, title, firstOfYear }) => (
             <li className={utilsStyles.listItem} key={id}>
+              <Year year={firstOfYear} />
               <Link href={`/blog/${id}`}>
                 <a>{title}</a>
               </Link>
