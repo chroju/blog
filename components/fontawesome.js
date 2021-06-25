1d1796cf2c3addbeefd24508852e7c79ef31ba6b
chroju <chroju@users.noreply.github.com>
2021-06-26 00:28:06 +0900

    fix: fontawesome css config

--- components/fontawesome.js
+++ components/fontawesome.js
@@ -1,9 +1,10 @@
-import { library } from '@fortawesome/fontawesome-svg-core'
+import { library, config } from '@fortawesome/fontawesome-svg-core'
 import { faGithub, faKeybase, faSpeakerDeck, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons'
 import { faRssSquare, faTags } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default function Fa({ iconPrefix = 'fab', iconName, title }) {
+    config.autoAddCss = false;
     library.add(faGithub, faKeybase, faSpeakerDeck, faTwitter, faInstagram, faRssSquare, faTags)
     return <FontAwesomeIcon icon={[iconPrefix, iconName]} title={title} size='xs' />
 }
