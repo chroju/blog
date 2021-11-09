6ebe6c8ebdec6b40efc9103eceb3d1000c70b828
chroju <chroju@users.noreply.github.com>
2021-11-09 18:40:34 +0900

    add: LICENSE

--- pages/policy.js
+++ pages/policy.js
@@ -28,6 +28,12 @@ export default function Home() {
                     <p><small>このサイトでは <Link href="https://www.google.com/analytics"><a>Google Analytics</a></Link> を使用しています。 Google Analytics は、本サイト利用者のブラウザに cookie を付与することにより、利用者の訪問履歴を収集、分析します。収集する情報には、利用者個人を識別する情報は含まれません。利用者はブラウザの cookie 設定により、データの収集を拒否することができます。詳細は <Link href="https://marketingplatform.google.com/about/analytics/terms/jp/"><a>Google Analytics 利用規約</a></Link> を参照してください。</small></p>
                 </ul>
             </section>
+            <section className={utilsStyles.headingMd}>
+                <h2 className={utilsStyles.headingLg}>LICENSE</h2>
+                <ul>
+                    <p><small>このサイト内の文章、ソースコードは断りがない限り <Link href="https://github.com/chroju/blog/blob/main/LICENSE"><a>MIT License</a></Link> で公開しています。</small></p>
+                </ul>
+            </section>
         </Layout>
     )
 }
