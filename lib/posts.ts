4ace1d988422923d15b4497760af5e104059ec49
chroju <chroju@users.noreply.github.com>
2022-05-05 11:16:03 +0900

    [ts-migrate][lib] Run TS Migrate
    Co-authored-by: ts-migrate <>

--- lib/posts.ts
+++ lib/posts.ts
@@ -1,15 +1,18 @@
+// @ts-expect-error ts-migrate(2307) FIXME: Cannot find module 'fs' or its corresponding type ... Remove this comment to see the full error message
 import fs from 'fs'
+// @ts-expect-error ts-migrate(2307) FIXME: Cannot find module 'path' or its corresponding typ... Remove this comment to see the full error message
 import path from 'path'
 import matter from 'gray-matter'
 import remark from 'remark'
 import html from 'remark-html'
 import gfm from 'remark-gfm'
 
+// @ts-expect-error ts-migrate(2580) FIXME: Cannot find name 'process'. Do you need to install... Remove this comment to see the full error message
 const postsDirectory = path.join(process.cwd(), 'posts')
 
 export function getSortedPostsData() {
     const fileNames = fs.readdirSync(postsDirectory)
-    const allPostsData = fileNames.map(fileName => {
+    const allPostsData = fileNames.map((fileName: any) => {
         const id = fileName.replace(/\.md$/, '')
         const fullPath = path.join(postsDirectory, fileName)
         const fileContents = fs.readFileSync(fullPath, 'utf8')
@@ -21,7 +24,7 @@ export function getSortedPostsData() {
         }
     })
 
-    const sortedPosts = allPostsData.sort((a, b) => {
+    const sortedPosts = allPostsData.sort((a: any, b: any) => {
         if (a.date < b.date) {
             return 1
         } else {
@@ -30,52 +33,53 @@ export function getSortedPostsData() {
     })
 
     let previous = '0'
-    return sortedPosts.map(post => {
+    return sortedPosts.map((post: any) => {
         const year = post.date.substr(0, 4)
         post.firstOfYear = year !== previous ? year : null
         previous = year
         return post
-    })
+    });
 }
 
 
 export function getAllTags() {
     const allPostsData = getSortedPostsData()
-    const allTags = allPostsData.map(postData => {
-        return postData.tags === undefined || postData.tags[0] === '' ? [] : postData.tags.map(tag => {
+    const allTags = allPostsData.map((postData: any) => {
+        return postData.tags === undefined || postData.tags[0] === '' ? [] : postData.tags.map((tag: any) => {
             return encodeURI(tag)
-        })
+        });
     }).flat()
     const tagCounts = {}
-    allTags.forEach(tag => {
+    allTags.forEach((tag: any) => {
+        // @ts-expect-error ts-migrate(7053) FIXME: Element implicitly has an 'any' type because expre... Remove this comment to see the full error message
         tagCounts[tag] = (tagCounts[tag] || 0) + 1;
     })
     return tagCounts
 }
 
 
-export function getPostsWithTag(tag) {
+export function getPostsWithTag(tag: any) {
     const decodedTag = decodeURI(tag)
     const allPostsData = getSortedPostsData()
-    return allPostsData.filter(post => {
+    return allPostsData.filter((post: any) => {
         return post.tags !== undefined && post.tags.includes(decodedTag)
-    })
+    });
 }
 
 
 export function getAllPostIds() {
     const fileNames = fs.readdirSync(postsDirectory)
 
-    return fileNames.map(fileName => {
+    return fileNames.map((fileName: any) => {
         return {
             params: {
                 id: fileName.replace(/\.md$/, '')
             }
-        }
-    })
+        };
+    });
 }
 
-export async function getPostData(id) {
+export async function getPostData(id: any) {
     const fullPath = path.join(postsDirectory, `${id}.md`)
     const fileContents = fs.readFileSync(fullPath, 'utf8')
     const matterResult = matter(fileContents)
